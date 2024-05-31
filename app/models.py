e9aa516bf298630fd041470f0d702831f24873a8
Matthias Dive <matthiasdive2@gmail.com>
2024-05-31 15:20:42 +0200

    debug views.py, models.py + templates

--- app/models.py
+++ app/models.py
@@ -5,8 +5,12 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    year = models.DateField()
-    type = models.CharField(max_length=20)
+    year = models.CharField(max_length=4, null=True, blank=True)
+    type = models.CharField(null=True, blank=True, max_length=20)
+
+    @staticmethod
+    def format_year(year):
+        return year.split('-')[0] if '-' in year else year
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.year})"
--- app/views.py
+++ app/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 import requests
 import os
+from .forms import SearchForm
+from .models import Movie
+
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 def index(request):
-    return render(request, "index.html")
+    results = None
+    movies = None
+    form = SearchForm()
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["search"]
+            movies = Movie.objects.filter(title__icontains=title)
+            if not movies.exists():
+                results = get_request(title)
+                if results.get('Search') is not None:
+                    for result in results.get('Search'):
+                        formatted_year = Movie.format_year(result['Year'])
+                        Movie.objects.create(
+                            title=result['Title'],
+                            year=formatted_year,
+                            type=result['Type']
+                        )
+
+    return render(request, "index.html", {'form': form, 'results': results, 'movies': movies})
 
 
-def get_request():
-    url = f"http://www.omdbapi.com/?apikey={SECRET_KEY}&"
+def get_request(title):
+    url = f"http://www.omdbapi.com/?apikey={SECRET_KEY}&s={title}"
     res = requests.get(url=url)
-    print(res.status_code)
+    if res.status_code == 200:
+        return res.json()
+    else:
+        print(f"Error: {res.status_code}")
